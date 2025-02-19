8cbb2cffebe2d1bf79b279743a203e48593dd2fb
Chintu Kumar <icoderchintu@gmail.com>
2025-02-19 12:15:03 +0530

    61

--- first.js
+++ first.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     // Change the background color of the body
     document.body.style.backgroundColor = "pink";
     alert("Welcome to my world");
+});
 
- 
